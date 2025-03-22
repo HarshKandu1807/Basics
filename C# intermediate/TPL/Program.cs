9932a54c190ed81fb5a8cbd595b9dd7b6ef9dbc9
Harsh Kandu <harsh.kandu@nimapinfotech.com>
2025-03-22 18:59:47 +0530

    TPL

---
+++ C# intermediate/TPL/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task t1 = new Task(() => Dowork(1,2500));
+            //t1.Start();
+            //Task t2 = new Task(() => Dowork(2,2000));
+            //t2.Start();
+            //Task t3 = new Task(() => Dowork(3,1500));
+            //t3.Start();
+            Task t1 = Task.Factory.StartNew(() => Dowork(1, 2000)).ContinueWith((prev) => DoOtherwork(4,1000));//the DoOtherwork will only start when the task 1 is finshed
+            Task t2 = Task.Factory.StartNew(() => Dowork(2, 2500));//factory.startnew will automatically execute the task wahen it is assingned
+            Task t3 = Task.Factory.StartNew(() => Dowork(3, 1500));
+
+
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadKey();
+        }
+        static void Dowork(int id,int sleep)
+        {
+            Console.WriteLine("Task {0} begin",id);
+            Thread.Sleep(sleep);
+            Console.WriteLine("Task {0} end",id);
+
+        }
+        static void DoOtherwork(int id, int sleep)
+        {
+            Console.WriteLine("Task {0} begin", id);
+            Thread.Sleep(sleep);
+            Console.WriteLine("Task {0} end", id);
+
+        }
+    }
+}
---
+++ C# intermediate/out&ref/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace out_ref
+{
+    class Program
+    {
+        static void sf(ref int n,out int n1,int n2)//20,NULL,20
+        {
+            n1 = 0;//the n1 value given in caller is distroyed
+            n = n + 10;//20+10=30 and return to caller
+            n1 = n1 + 10;//0+10=10 and return to caller
+            n2 = n2 + 10;//20+10=30 and the value gets ditroyed and value do not go to caller
+        }
+        static void Main(string[] args)
+        {
+            int n = 20;
+            int n1 = 20;
+            int n2 = 20;
+            sf(ref n, out n1,n2);//ref value will be forwaded to calle and return back with new value to caller  and out value will be distroyed and n2 value wil also forward to calle but does not return to caller
+            Console.WriteLine(n+" "+n1+" "+n2);
+        }
+    }
+}
