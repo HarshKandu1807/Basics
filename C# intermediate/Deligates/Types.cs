d781c987bf8d35cf679d07ed1e4ab16f0d728b0b
Harsh Kandu <harsh.kandu@nimapinfotech.com>
2025-03-25 18:39:38 +0530

    InnerException&Delegates

---
+++ C# intermediate/Deligates/Types.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deligates
+{
+    class Types
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> addDelegate = (a,b)=>a+b;
+            Console.WriteLine("Sum: " + addDelegate(10,20));
+
+            Action<string> printDelegate = msg => Console.WriteLine(msg);
+            printDelegate("Hello");
+
+            Predicate<int> EvenDelegate = n => n%2==0;
+            Console.WriteLine("Is 4 even? " + EvenDelegate(4));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C# intermediate/InnerException/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnerException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                try
+                {
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    int n1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(n / n1);
+                }
+                catch (Exception e)
+                {
+                    string s = @"C:\Users\Nimap\source\repos\InnerException\log.txt";
+                    if (File.Exists(s))
+                    {
+                        StreamWriter sr = new StreamWriter(s);
+                        sr.Write(e.GetType().Name);
+                        sr.WriteLine();
+                        sr.Write(e.Message);
+                        sr.Close();
+                        Console.WriteLine("There is a problem");
+                    }
+                    else
+                    {
+                        throw new FileNotFoundException(s + " sis not present ", e);
+                    }
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine("Current Exception: {0}",ex.GetType().Name);
+                Console.WriteLine("Inner Exception: {0}" , ex.InnerException.GetType().Name);
+            }
+        }
+    }
+}
